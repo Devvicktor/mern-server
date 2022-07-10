ad572eca0ab77244c595cacfb90fc62b08560046
Devvicktor <kosgeivick90@gmail.com>
2022-07-10 08:39:25 +0300

    versioning og APIs

--- routes/pet.route.js
+++ routes/pet.route.js
@@ -2,10 +2,10 @@ const petController = require("../controllers/pet.controller");
 const PetController = require("../controllers/pet.controller");
 
 module.exports = (app) => {
-  app.get("/api", petController.index);
-  app.get("/api/pets", PetController.getPets);
-  app.get("/api/pets/:id", PetController.getPet);
-  app.post("/api/pets", PetController.createPet);
-  app.put("/api/pets/:id", PetController.updatePet);
-  app.delete("/api/pets/:id", PetController.deletePet);
+  app.get("/api/v1", petController.index);
+  app.get("/api/v1/pets", PetController.getPets);
+  app.get("/api/v1/pets/:id", PetController.getPet);
+  app.post("/api/v1/pets", PetController.createPet);
+  app.put("/api/v1/pets/:id", PetController.updatePet);
+  app.delete("/api/v1/pets/:id", PetController.deletePet);
 };
